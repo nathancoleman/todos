ec18c8be7a362410de5b0644ef72c45bcb8eb03c
Nathan Coleman <nathandanielcoleman@gmail.com>
2024-01-20 13:17:05 -0500

    Configure logger, add run instruction to README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TODOs
 My first [htmx](https://htmx.org/) app, backed by https://dummyjson.com/todos
+
+```shell
+$ go run main.go
+```
--- main.go
+++ main.go
@@ -9,6 +9,17 @@ import (
 )
 
 func main() {
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				return slog.Attr{}
+			}
+			return a
+		},
+	})))
+	slog.Info("Serving on http://localhost:8080/todos")
+
 	if err := http.ListenAndServe(":8080", routes.Router()); err != nil {
 		slog.Warn("Stopped serving", "error", err.Error())
 		os.Exit(1)
